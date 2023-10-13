1a3d2cd8f75c2ddeb7de21d5bfe47485db811ed7
sunmin <sunmin0406@naver.com>
2023-10-13 17:03:29 +0900

    관리자 기능 CRUD 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // api 버전 관리
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+
   await app.listen(3000);
 }
 bootstrap();
